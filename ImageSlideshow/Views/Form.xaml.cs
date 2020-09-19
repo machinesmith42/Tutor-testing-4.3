3d0f68250df73083842acdb23ffc03c4cd9b6fea
Jason Wibowo <jwibowo@butlercc.edu>
2020-09-18 20:08:20 -0500

    Form 50% done

--- ImageSlideshow/Views/Form.xaml.cs
+++ ImageSlideshow/Views/Form.xaml.cs
@@ -25,6 +25,7 @@ namespace ImageSlideshow.Views {
         private static readonly TutorDataSet.AllTutorsDataTable tutorTable = new TutorDataSet.AllTutorsDataTable();
         private static readonly TutorDataSet.ScheduleDataTable scheduleTable = new TutorDataSet.ScheduleDataTable();
         private static readonly TutorDataSet.SubjectDataTable subjectTable = new TutorDataSet.SubjectDataTable();
+        
         public Form() {
             AllTutorsTableAdapter tutorTableAdapt = new AllTutorsTableAdapter();
             tutorTableAdapt.Fill(tutorTable);
@@ -36,18 +37,41 @@ namespace ImageSlideshow.Views {
             scheduleAdapt.Dispose();
             subjectAdapt.Dispose();
             InitializeComponent();
-            this.Topmost = true;
+            Topmost = true;
+            
         }
 
         private void Button_Click(object sender, RoutedEventArgs e) {
             this.Close();
         }
-
+        private void Form_Load(object sender, RoutedEventArgs e) {
+            subjects.Items.Add(new ComboBoxItem<string>("All", "all"));
+            var query = (
+                from subject in subjectTable.AsEnumerable()
+                select new {
+                    Name = subject.Field<string>("TutorSubject"),
+                    Value = subject.Field<string>("TutorSubject")
+                }
+            ).Distinct();
+            foreach(var q in query) {
+                subjects.Items.Add(new ComboBoxItem<string>(q.Name, q.Value));
+            }
+            weekdays.Items.Add(new ComboBoxItem<int>("Sunday", 1));
+            weekdays.Items.Add(new ComboBoxItem<int>("Monday", 2));
+            weekdays.Items.Add(new ComboBoxItem<int>("Tuesday", 3));
+            weekdays.Items.Add(new ComboBoxItem<int>("Wednesday", 4));
+            weekdays.Items.Add(new ComboBoxItem<int>("Thursday", 5));
+            weekdays.Items.Add(new ComboBoxItem<int>("Friday", 6));
+            weekdays.Items.Add(new ComboBoxItem<int>("Saturday", 1));
+        }
         private void Submit_Click(object sender, RoutedEventArgs e) {
             bool isCampusChecked = false;
+            bool isSubjectSelected = false;
+            bool isWeekdaySelected = false;
+            bool isTimeSelected = false;
             string[] campus = new string[3]; 
             if ((bool)andoverCheck.IsChecked) {
-                campus[0] = "Andover";
+              campus[0] = "Andover";
                 isCampusChecked = true;
             }
             if ((bool)eldorado.IsChecked) {
@@ -58,13 +82,23 @@ namespace ImageSlideshow.Views {
                 campus[2] = "Online";
                 isCampusChecked = true;
             }
-
+            if (subjects.SelectedItems.Count > 0) {
+                isSubjectSelected = true;
+            }
+            if (weekdays.SelectedItems.Count > 0) {
+                isWeekdaySelected = true;
+            }
+            if(time.Value.HasValue) {
+                isTimeSelected = true;
+            }
             var query =
                 from schedule in scheduleTable.AsEnumerable()
                 join subject in subjectTable
                 on schedule.Field<int>("ID") equals subject.Field<int>("ID")
-                let v = (campus.Contains(schedule.Field<string>("campus")))
-                where (v && isCampusChecked || !isCampusChecked)
+                where (campus.Contains(schedule.Field<string>("campus")) && isCampusChecked || !isCampusChecked) && 
+                      (subjects.SelectedValue.Split(',').ToList().Contains(subject.Field<string>("TutorSubject")) && isSubjectSelected || !isSubjectSelected) &&
+                      (weekdays.SelectedValue.Split(',').ToList().Contains(schedule.Field<int>("Day").ToString()) && isWeekdaySelected || !isWeekdaySelected) &&
+                      (TimeBetween((DateTime)time.Value, schedule.Field<DateTime>("Start").TimeOfDay, schedule.Field<DateTime>("End").TimeOfDay) && isTimeSelected || !isTimeSelected)
                 orderby schedule.Field<int>("ID")
                 select new {
                     TutorID = schedule.Field<int>("ID")
@@ -73,6 +107,30 @@ namespace ImageSlideshow.Views {
                 Debug.Print(q.TutorID.ToString()+"\n");
             }
         }
+        static bool TimeBetween(DateTime datetime, TimeSpan start, TimeSpan end) {
+            // convert datetime to a TimeSpan
+            TimeSpan now = datetime.TimeOfDay;
+            // see if start comes before end
+            if (start < end)
+                return start <= now && now <= end;
+            // start is after end, so do the inverse comparison
+            return !(end < now && now < start);
+        }
     }
-    
+    public class ComboBoxItem<T> {
+        private string Text { get; set; }
+        public T Value { get; set; }
+
+        public override string ToString() {
+            return Text;
+        }
+
+        public ComboBoxItem(string text, T value) {
+            Text = text;
+            Value = value;
+        }
+
+    }
+
+
 }
